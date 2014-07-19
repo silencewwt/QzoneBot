7b6cf2a8d441c1b172a156440eab6b0c4b67fb85
Silence-WWT <damn00good@gmail.com>
2014-07-19 19:23:22 +0800

    comment automaticly

--- AutoDoLike.py
+++ AutoDoLike.py
@@ -2,8 +2,9 @@
 # -*-coding:utf-8-*-
 
 import os
-import sys
 import re
+import sys
+import time
 import zlib
 import sqlite3
 import urllib.parse
@@ -15,19 +16,23 @@ import win32crypt
 
 
 class AutoDoLike(object):
-    def __init__(self, self_id, target_id):
+    def __init__(self, self_id, target_id, comment='Python大法好，用过都说屌!'):
         if self_id == '' or target_id == '':
             raise Exception('please input correct id!')
         self.self_id = self_id
         self.target_id = target_id
-        self.done = False
+        self.comment = comment
+        self.like_done = False
+        self.comment_done = False
+        self.prove = 0
         self.msg_list = []
         self.pages = 0
-        self.done_num = 0
+        self.like_num = 0
+        self.comment_num = 0
         self.url = 'http://taotao.qq.com/cgi-bin/emotion_cgi_msglist_v6?uin={}&inCharset=utf-8&outCharset=utf-8&' \
                    'hostUin={}&notice=0&sort=0&pos={}&num=20&cgi_host=http%3A%2F%2Ftaotao.qq.com%2F' \
                    'cgi-bin%2Femotion_cgi_msglist_v6&code_version=1&format=jsonp&need_private_comment=1' \
-                   '&g_tk=2112206370'
+                   '&g_tk={}'
         self.cookie = http.cookies.SimpleCookie()
         self.cookie_dic = {}
         self.cookie_name = ['__Q_w_s_hat_seed', '__Q_w_s__QZN_TodoMsgCnt', '__Q_w_s__appDataSeed', 'randomSeed',
@@ -47,9 +52,7 @@ class AutoDoLike(object):
             raise Exception('please setup Chrome first!')
         conn = sqlite3.connect(self.cookie_file_path)
         for domain in self.domain:
-            for row in conn.execute(
-                    'SELECT host_key, name, encrypted_value FROM cookies WHERE host_key=?',
-                    (domain,)):
+            for row in conn.execute('select host_key, name, encrypted_value from cookies where host_key=?', (domain,)):
                 pwd_hash = row[2]
                 try:
                     ret = win32crypt.CryptUnprotectData(pwd_hash, None, None, None, 0)
@@ -64,6 +67,12 @@ class AutoDoLike(object):
                 cookie_list.append('%s=%s; ' % (key, self.cookie_dic[key]))
         self.cookie_str = ''.join(cookie_list)
         conn.close()
+        prove_str = self.cookie_dic['skey']
+        variable_a = 5381
+        for char in prove_str:
+            variable_a += (variable_a << 5) + ord(char)
+        self.prove = variable_a & 2147483647
+        print(self.prove)
 
     def get_msg_id(self):
         headers = {'Accept': '*/*', 'Accept-Encoding': 'gzip,deflate,sdch', 'Accept-Language': 'zh-CN,zh;q=0.8',
@@ -73,8 +82,8 @@ class AutoDoLike(object):
                                  'Chrome/36.0.1985.125 Safari/537.36}'}
         reg = r'"t1_termtype":\w+,"tid":"([a-fA-F\d]+)","ugc_right"'
         reg = re.compile(reg)
-        while self.done is False:
-            url = self.url.format(self.target_id, self.target_id, self.pages*20)
+        while self.comment_done is False:
+            url = self.url.format(self.target_id, self.target_id, self.pages*20, self.prove)
             req = urllib.request.Request(url, headers=headers)
             resp_html = urllib.request.urlopen(req).read()
             resp_html = zlib.decompress(resp_html, 16 + zlib.MAX_WBITS).decode('utf8')
@@ -84,12 +93,13 @@ class AutoDoLike(object):
                 if tid not in msg_list:
                     self.msg_list.append(tid)
                     msg_list.append(tid)
-            self.post(msg_list)
+            # self.like_post(msg_list)
+            self.comment_post(msg_list)
             self.pages += 1
             print('%s pages done!' % self.pages)
-            self.work_done()
+        self.work_done()
 
-    def post(self, target_list):
+    def like_post(self, target_list):
         headers = {'Host': 'w.cnc.qzone.qq.com', 'Connection': 'keep-alive', 'Content-Length': '268',
                    'Cache-Control': 'max-age=0', 'Origin': 'http://user.qzone.qq.com',
                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -98,8 +108,8 @@ class AutoDoLike(object):
                    'Content-Type': 'application/x-www-form-urlencoded', 'Accept-Encoding': 'gzip,deflate,sdch',
                    'Referer': 'http://user.qzone.qq.com/%s' % self.target_id, 'Accept-Language': 'zh-CN,zh;q=0.8',
                    'Cookie': self.cookie_str}
-        if not target_list != []:
-            self.done = True
+        if target_list == []:
+            self.like_done = True
             return
         for tid in target_list:
             values = {'qzreferrer': 'http://user.qzone.qq.com/%s' % self.target_id, 'opuin': '%s' % self.self_id,
@@ -107,13 +117,41 @@ class AutoDoLike(object):
                       'curkey': 'http://user.qzone.qq.com/%s/mood/%s.1' % (self.target_id, tid), 'from': '-100',
                       'fupdate': '1', 'face': '0'}
             data = urllib.parse.urlencode(values).encode('utf8')
-            target_url = 'http://w.cnc.qzone.qq.com/cgi-bin/likes/internal_dolike_app?g_tk=2112206370'
+            target_url = 'http://w.cnc.qzone.qq.com/cgi-bin/likes/internal_dolike_app?g_tk=%s' % self.prove
             req = urllib.request.Request(target_url, headers=headers, data=data)
             urllib.request.urlopen(req).read()
-            self.done_num += 1
+
+            # time.sleep(1)
+            self.like_num += 1
+
+    def comment_post(self, target_list):
+        comment_url = 'http://taotao.qq.com/cgi-bin/emotion_cgi_re_feeds?g_tk=%s&' % self.prove
+        headers = {'Host': 'taotao.qq.com', 'Connection': 'keep-alive', 'Content-Length': '303',
+                   'Origin': 'http://user.qzone.qq.com', 'Accept': '*/*', 'Accept-Encoding': 'gzip,deflate,sdch',
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                 'Chrome/36.0.1985.125 Safari/537.36',
+                   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                   'Accept-Language': 'zh-CN,zh;q=0.8', 'Referer': 'http://user.qzone.qq.com/%s' % self.target_id,
+                   'Cookie': self.cookie_str + 'qqmusic_uin=; qqmusic_key=; qqmusic_fromtag=;'}
+        values = {'feedsType': '100', 'inCharset': 'utf-8', 'private': '0', 'paramstr': '2',
+                  'outCharset': 'utf-8', 'plat': 'qzone', 'source': 'ic', 'hostUin': self.target_id, 'isSignIn': '',
+                  'uin': self.self_id, 'format': 'fs', 'ref': 'feeds', 'content': self.comment, 'richval': '',
+                  'richtype': '', 'qzreferrer': 'http://user.qzone.qq.com/%s' % self.target_id}
+        if target_list == []:
+            self.comment_done = True
+            return
+        for tid in target_list:
+            values['topicId'] = '%s_%s_1' % (self.target_id, tid)
+            data = urllib.parse.urlencode(values).encode('utf8')
+            req = urllib.request.Request(comment_url, data=data, headers=headers)
+            resp_html = urllib.request.urlopen(req).read()
+            resp_html = zlib.decompress(resp_html, 16 + zlib.MAX_WBITS).decode('utf8')
+            with open('resp%s.htm' % self.comment_num, 'w', encoding='utf8') as html:
+                html.write(resp_html)
+            self.comment_num += 1
 
     def work_done(self):
-        print('totally %s pieces of messages done!' % self.done_num)
+        print('totally %s pieces of messages done!' % self.like_num)
         print('totally %s pages of messages!' % self.pages)
 
     def main(self):
