a20155a816e06b70867f6b09a2a8748362589cc3
Silence-WWT <damn00good@gmail.com>
2014-07-21 15:42:41 +0800

    增加初步自动队形功能

--- AutoDoLike.py
+++ AutoDoLike.py
@@ -10,6 +10,7 @@ import sqlite3
 import urllib.parse
 import urllib.request
 import urllib.error
+import http.client
 import http.cookies
 import http.cookiejar
 import win32crypt
@@ -72,7 +73,6 @@ class AutoDoLike(object):
         for char in prove_str:
             variable_a += (variable_a << 5) + ord(char)
         self.prove = variable_a & 2147483647
-        print(self.prove)
 
     def get_msg_id(self):
         headers = {'Accept': '*/*', 'Accept-Encoding': 'gzip,deflate,sdch', 'Accept-Language': 'zh-CN,zh;q=0.8',
@@ -80,21 +80,40 @@ class AutoDoLike(object):
                    'Referer': 'http://cnc.qzs.qq.com/qzone/app/mood_v6/html/index.html',
                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
                                  'Chrome/36.0.1985.125 Safari/537.36}'}
-        reg = r'"t1_termtype":\w+,"tid":"([a-fA-F\d]+)","ugc_right"'
-        reg = re.compile(reg)
         while self.comment_done is False:
             url = self.url.format(self.target_id, self.target_id, self.pages*20, self.prove)
             req = urllib.request.Request(url, headers=headers)
-            resp_html = urllib.request.urlopen(req).read()
+            try:
+                resp_html = urllib.request.urlopen(req).read()
+            except urllib.error.HTTPError as e:
+                print(e.code, e.reason)
+                sys.exit(-1)
+            except urllib.error.URLError as e:
+                print(e.reason)
+                sys.exit(-1)
             resp_html = zlib.decompress(resp_html, 16 + zlib.MAX_WBITS).decode('utf8')
-            target_list = re.findall(reg, resp_html)
+            page_data = {}
+            try:
+                page_data = eval(resp_html[10:-2])
+            except NameError as e:
+                print(e.__cause__)
+            target_list = []
+            for msg in page_data['msglist']:
+                target_list.append(msg['tid'])
             msg_list = []
             for tid in target_list:
                 if tid not in msg_list:
                     self.msg_list.append(tid)
                     msg_list.append(tid)
+            for msg in page_data['msglist']:
+                if msg['cmtnum'] > 1:
+                    comment_list = []
+                    for cmt in msg['commentlist']:
+                        comment_list.append(cmt['content'])
+                    self.imitate_post(msg['tid'], comment_list)
             # self.like_post(msg_list)
-            self.comment_post(msg_list)
+            # self.comment_post(msg_list)
+            # self.get_more_comment(msg_list)
             self.pages += 1
             print('%s pages done!' % self.pages)
         self.work_done()
@@ -108,47 +127,117 @@ class AutoDoLike(object):
                    'Content-Type': 'application/x-www-form-urlencoded', 'Accept-Encoding': 'gzip,deflate,sdch',
                    'Referer': 'http://user.qzone.qq.com/%s' % self.target_id, 'Accept-Language': 'zh-CN,zh;q=0.8',
                    'Cookie': self.cookie_str}
-        if target_list == []:
+        if not target_list:
             self.like_done = True
             return
         for tid in target_list:
-            values = {'qzreferrer': 'http://user.qzone.qq.com/%s' % self.target_id, 'opuin': '%s' % self.self_id,
-                      'unikey': 'http://user.qzone.qq.com/%s/mood/%s.1' % (self.target_id, tid),
-                      'curkey': 'http://user.qzone.qq.com/%s/mood/%s.1' % (self.target_id, tid), 'from': '-100',
-                      'fupdate': '1', 'face': '0'}
-            data = urllib.parse.urlencode(values).encode('utf8')
+            data = {'qzreferrer': 'http://user.qzone.qq.com/%s' % self.target_id, 'opuin': '%s' % self.self_id,
+                    'unikey': 'http://user.qzone.qq.com/%s/mood/%s.1' % (self.target_id, tid),
+                    'curkey': 'http://user.qzone.qq.com/%s/mood/%s.1' % (self.target_id, tid), 'from': '-100',
+                    'fupdate': '1', 'face': '0'}
+            data = urllib.parse.urlencode(data).encode('utf8')
             target_url = 'http://w.cnc.qzone.qq.com/cgi-bin/likes/internal_dolike_app?g_tk=%s' % self.prove
             req = urllib.request.Request(target_url, headers=headers, data=data)
-            urllib.request.urlopen(req).read()
-
-            # time.sleep(1)
+            try:
+                urllib.request.urlopen(req)
+            except urllib.error.HTTPError as e:
+                print(e.code, e.reason)
+            except urllib.error.URLError as e:
+                print(e.reason)
+            time.sleep(1)
             self.like_num += 1
 
-    def comment_post(self, target_list):
+    def comment_post(self, target_list, comment=''):
         comment_url = 'http://taotao.qq.com/cgi-bin/emotion_cgi_re_feeds?g_tk=%s&' % self.prove
-        headers = {'Host': 'taotao.qq.com', 'Connection': 'keep-alive', 'Content-Length': '303',
+        headers = {'Host': 'taotao.qq.com', 'Connection': 'keep-alive', 'Content-Length': '286',
                    'Origin': 'http://user.qzone.qq.com', 'Accept': '*/*', 'Accept-Encoding': 'gzip,deflate,sdch',
                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
                                  'Chrome/36.0.1985.125 Safari/537.36',
                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
                    'Accept-Language': 'zh-CN,zh;q=0.8', 'Referer': 'http://user.qzone.qq.com/%s' % self.target_id,
                    'Cookie': self.cookie_str + 'qqmusic_uin=; qqmusic_key=; qqmusic_fromtag=;'}
-        values = {'feedsType': '100', 'inCharset': 'utf-8', 'private': '0', 'paramstr': '2',
+        values = {'topicId': '', 'feedsType': '100', 'inCharset': 'utf-8', 'private': '0', 'paramstr': '2',
                   'outCharset': 'utf-8', 'plat': 'qzone', 'source': 'ic', 'hostUin': self.target_id, 'isSignIn': '',
-                  'uin': self.self_id, 'format': 'fs', 'ref': 'feeds', 'content': self.comment, 'richval': '',
-                  'richtype': '', 'qzreferrer': 'http://user.qzone.qq.com/%s' % self.target_id}
-        if target_list == []:
-            self.comment_done = True
-            return
+                  'uin': self.self_id, 'format': 'fs', 'ref': 'feeds', 'richval': '', 'richtype': '',
+                  'qzreferrer': 'http://user.qzone.qq.com/%s' % self.target_id}
+        if not comment:
+            if not target_list:
+                self.comment_done = True
+                return
         for tid in target_list:
             values['topicId'] = '%s_%s_1' % (self.target_id, tid)
+            if comment:
+                values['content'] = comment
+            else:
+                values['content'] = self.comment
             data = urllib.parse.urlencode(values).encode('utf8')
             req = urllib.request.Request(comment_url, data=data, headers=headers)
-            resp_html = urllib.request.urlopen(req).read()
-            resp_html = zlib.decompress(resp_html, 16 + zlib.MAX_WBITS).decode('utf8')
-            with open('resp%s.htm' % self.comment_num, 'w', encoding='utf8') as html:
-                html.write(resp_html)
+            try:
+                urllib.request.urlopen(req)
+            except urllib.error.HTTPError as e:
+                print(e.code, e.reason)
+            except urllib.error.URLError as e:
+                print(e.reason)
             self.comment_num += 1
+
+    def get_more_comment(self, tid):
+        get_url = 'http://taotao.qq.com/cgi-bin/emotion_cgi_ic_getcomments?g_tk=%s' % self.prove
+        headers = {'Host': 'taotao.qq.com', 'Connection': 'keep-alive', 'Content-Length': '613',
+                   'Cache-Control': 'max-age=0', 'Accept': 'text/html,application/xhtml+xml,application'
+                                                           '/xml;q=0.9,image/webp,*/*;q=0.8',
+                   'Origin': 'http://ic2.s8.qzone.qq.com',
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome'
+                                 '/36.0.1985.125 Safari/537.36',
+                   'Content-Type': 'application/x-www-form-urlencoded',
+                   'Accept-Encoding': 'gzip,deflate,sdch', 'Accept-Language': 'zh-CN,zh;q=0.8',
+                   'Cookie': self.cookie_str + 'qqmusic_uin=; qqmusic_key=; qqmusic_fromtag=;'}
+        values = {'topicId': '', 'feedsType': '8', 'inCharset': 'gbk', 'outCharset': 'gbk', 'sort': '1',
+                  'plat': 'qzone', 'source': 'ic', 'hostUin': self.target_id, 'uin': self.self_id, 'start': '0',
+                  'num': '20', 'format': 'fs', 'ref': 'feeds', 'isfakereq': '1', 'paramstr': '2'}
+        referer = 'http://ic2.s8.qzone.qq.com/cgi-bin/feeds/feeds_html_module?i_uin={}&i_login_uin={}&mode=4&' \
+                  'previewV8=1&style=35&version=8&needDelOpr=true&transparence=true&hideExtend=false&showcount=5&' \
+                  'MORE_FEEDS_CGI=http%3A%2F%2Fic2.s8.qzone.qq.com%2Fcgi-bin%2Ffeeds%2Ffeeds_html_act_all&refer=2&' \
+                  'paramstring=os-win7|100'
+        referer = referer.format(self.target_id, self.self_id)
+        headers['Referer'] = referer
+        values['qzreferrer'] = referer
+        values['topicId'] = '%s_%s__1' % (self.target_id, tid)
+        data = urllib.parse.urlencode(values).encode('utf8')
+        req = urllib.request.Request(get_url, headers=headers, data=data)
+        resp_html = ''
+        try:
+            resp_html = urllib.request.urlopen(req).read()
+        except http.client.HTTPException as e:
+            print(e.__cause__)
+        except urllib.error.HTTPError as e:
+            print(e.code, e.reason)
+        except urllib.error.URLError as e:
+            print(e.reason)
+        if resp_html:
+            resp_html = zlib.decompress(resp_html, 16 + zlib.MAX_WBITS).decode('utf8')
+            reg = re.compile(r'&nbsp; : (.+?)\\u003Cdiv')
+            content_list = re.findall(reg, resp_html)
+            if content_list:
+                self.imitate_post(tid, content_list)
+
+    def imitate_post(self, tid, comment_list):
+        cmt_list = []
+        print(comment_list)
+        for i in range(len(comment_list)):
+            for j in range(len(comment_list)):
+                if i != j:
+                    if comment_list[i] == comment_list[j]:
+                        # self.comment_post([tid], comment_list[i])
+                        return
+        for comment in comment_list:
+            comment = re.sub(r'\[em\]e\w+\[\\/em\]', '', comment)
+            comment = re.sub(r'@\{uin:\d+,nick:.+?,who:\d+\}', '', comment)
+            m = re.findall('(\w+)', comment)
+            if m:
+                cmt_list.append(''.join(m))
+        print(cmt_list)
+        for comment in cmt_list:
+            pass
 
     def work_done(self):
         print('totally %s pieces of messages done!' % self.like_num)
@@ -158,5 +247,7 @@ class AutoDoLike(object):
         self.get_cookies()
         self.get_msg_id()
 
-a = AutoDoLike('', '')
-a.main()
+
+a = AutoDoLike('', '', '……')
+if __name__ == '__main__':
+    a.main()
